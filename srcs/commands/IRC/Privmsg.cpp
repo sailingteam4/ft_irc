5978371b645abfba61e45bff0bc26fce821cabe8
SailingTeam4 <sailing4team@gmail.com>
2025-04-29 18:19:05 +0200

    change ip for global in ddc send

--- srcs/commands/IRC/Privmsg.cpp
+++ srcs/commands/IRC/Privmsg.cpp
@@ -163,7 +163,7 @@ void Server::handleDccSend(int client_fd, const std::string& nickname, const std
     
     if (ipAddress.empty())
     {
-        ipAddress = "127.0.0.1";
+        ipAddress = "0.0.0.0";
         port = "0"; 
     }
     
